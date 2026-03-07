5d1e3d3855b3f0064713ed32da3a7cb2c7f00dd9
Prince Nkansah <nkansahp466@gmail.com>
2026-03-07 10:41:56 +0000

    Fix: move DB credentials to .env file

--- config/db.py
+++ config/db.py
@@ -1,10 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 DB_CONFIG ={
-    "host":"localhost",
-    "user": "Admin",
-    "password":"12345",
+    "host":os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password":os.getenv("DB_PASSWORD"),
+    "database":os.getenv("DB_NAME")
 }
 
 def get_connection():
