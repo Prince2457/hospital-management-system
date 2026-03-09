67a11ed7cd39394760cf681e5f096e44e11a67b2
Prince Nkansah <nkansahp466@gmail.com>
2026-03-09 13:23:12 +0000

    Add create_patient() and get_patient_by_id() functions

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from config.db import get_connection, close_connection
-from utils.patients import get_all_patients
+from utils.patients import get_all_patients, get_patient_by_id ,create_patient
 def main():
     print("🏥 Hospital Management System")
     print("=" * 40)
 
+
+
     patients = get_all_patients()
-
-    if patients:
-        print(f"Found {len(patients)} patients:")
-        for patient in patients:
-            print(patient)
-
-    else:
-        print("NO patients found")    
+    for patient in patients:
+        print(patient['patient_id'], '-', patient['full_name'])
     
-    
+
 
 if __name__ == "__main__":
     main()
--- utils/patients.py
+++ utils/patients.py
@@ -6,15 +6,64 @@ def get_all_patients():
     
     if not connection :
         return []
-    cursor = connection.cursor(dictionary=True)
+    cursor = connection.cursor(dictionary=True, buffered=True)
 
     try:
         cursor.execute("SELECT * FROM patients")
-        patients = cursor.fetchmany()
+        patients = cursor.fetchall()
         return patients
     
     except Exception as e:
         print(f"❌ Error fetching patients: {e}")
         return []
     finally:
-        close_connection(connection, cursor)   
+        close_connection(connection, cursor)  
+
+def get_patient_by_id(patient_id):
+    connection = get_connection()
+
+    if not connection:
+        return None
+
+    cursor = connection.cursor(dictionary=True)
+
+    try:
+        cursor.execute("SELECT * FROM patients WHERE patient_id =%s",(patient_id,))
+        patient = cursor.fetchone()
+        return patient
+    except Exception as e:
+        print(f"❌ Error fetching patient: {e}")
+        return None
+    finally:
+        close_connection(connection, cursor)
+
+def create_patient(full_name, ghana_card_number, date_of_birth, gender,
+                phone, email, address, region, blood_group,
+                nhis_number, nhis_expiry, emergency_contact_name,
+                emergency_contact_phone, registered_by): 
+    connection = get_connection()
+    if not connection:
+        return False
+    
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("""INSERT INTO patients (full_name, ghana_card_number, date_of_birth, gender,
+                phone, email, address, region, blood_group,
+                nhis_number, nhis_expiry, emergency_contact_name,
+                emergency_contact_phone, registered_by
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                (full_name, ghana_card_number, date_of_birth, gender,
+                phone, email, address, region, blood_group,
+                nhis_number, nhis_expiry, emergency_contact_name,
+                emergency_contact_phone, registered_by))
+        
+        connection.commit()
+        print(f"✅ Patient {full_name} created successfully!")
+        return True
+    except Exception as e:
+        print(f"❌ Error creating patient: {e}")
+        connection.rollback()
+        return False
+    finally:
+        close_connection(connection, cursor)
